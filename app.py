7b74b187390eae4fae9517e6ed798b8350a79ad2
mohammadrezarouhani <rezarouhanitonekaboni@gmail.com>
2022-12-31 03:11:54 -0700

    file for lexical analizing created

---
+++ app.py
@@ -0,0 +1,26 @@
+
+a="hello world"
+b="python lexical analizer"
+c=20
+d=10
+e=20
+
+class compiler:
+    def __init__(self) -> None:
+        self.name="compiler class"
+        self.value=1
+
+    def sub(self,a,b):
+        return a-b
+
+def add(a,b):
+    return a+b
+
+for i in range(0,10):
+    print(i)
+
+while(c):
+    c=c-1
+    d=d+1
+    e=e+1
+    
---
+++ main.py
@@ -0,0 +1,6 @@
+import pdb
+a=""
+
+with open(".\\app.py",'r') as file:
+    print(file.readlines())
+    
